cdd99e79513f1112c7391d95211e1ccaa58f5754
haeyong9701 <haeyong9701@gmail.com>
2024-02-27 16:04:22 +0900

    feat: API 리퀘스트 리스폰스

--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -1,3 +1,33 @@
 export default function handler(req, res) {
-  res.send("안녕 다이나믹 라우트!");
+  switch (req.method) {
+    case "POST":
+      res.status(201).send({
+        title: "네이버",
+        url: "https://www.naver.com",
+      });
+      break;
+
+    case "GET":
+      res.send([
+        {
+          id: 1,
+          title: "어딘가1",
+          url: "https://www.wswwww.com",
+        },
+        {
+          id: 2,
+          title: "어딘가2",
+          url: "https://www.aaaaa.com",
+        },
+        {
+          id: 3,
+          title: "어딘가3",
+          url: "https://www.bbbb.com",
+        },
+      ]);
+      break;
+
+    default:
+      res.status(404).send();
+  }
 }
